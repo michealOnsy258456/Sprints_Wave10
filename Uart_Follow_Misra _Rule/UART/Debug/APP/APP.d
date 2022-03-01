54c9d92006ad6095ef6ade64a6e7dc05939b2f5f
Micheal Onsy <micheal.onsy.helmy@gmail.com>
2022-03-01 20:16:01 +0200

    Uart Driver Follow Misra Rula & Presentation

---
+++ Uart_Follow_Misra _Rule/UART/Debug/APP/APP.d
@@ -0,0 +1,14 @@
+APP/APP.o APP/APP.o: ../APP/APP.c ../APP/APP.h ../APP/../MCAL/UART/UART.h \
+  ../APP/../MCAL/UART/../../LIBRARY/Std_types.h \
+  ../APP/../MCAL/UART/../../LIBRARY/common_macros.h \
+  ../APP/../MCAL/UART/../../LIBRARY/Register.h
+
+../APP/APP.h:
+
+../APP/../MCAL/UART/UART.h:
+
+../APP/../MCAL/UART/../../LIBRARY/Std_types.h:
+
+../APP/../MCAL/UART/../../LIBRARY/common_macros.h:
+
+../APP/../MCAL/UART/../../LIBRARY/Register.h:
---
+++ Uart_Follow_Misra _Rule/UART/Debug/MCAL/UART/UART.d
@@ -0,0 +1,12 @@
+MCAL/UART/UART.o MCAL/UART/UART.o: ../MCAL/UART/UART.c \
+  ../MCAL/UART/UART.h ../MCAL/UART/../../LIBRARY/Std_types.h \
+  ../MCAL/UART/../../LIBRARY/common_macros.h \
+  ../MCAL/UART/../../LIBRARY/Register.h
+
+../MCAL/UART/UART.h:
+
+../MCAL/UART/../../LIBRARY/Std_types.h:
+
+../MCAL/UART/../../LIBRARY/common_macros.h:
+
+../MCAL/UART/../../LIBRARY/Register.h:
